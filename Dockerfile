e76f02298e870d6944f858b105f56722115efb7d
RofloZavr <55450094+RofloZavr@users.noreply.github.com>
2019-09-18 14:53:43 +0300

    Revert "2019 09 raiffeisen"

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM fabric8/java-alpine-openjdk8-jre:1.6.3
-EXPOSE 8080
-COPY target/dbo-1.0-SNAPSHOT.jar /usr/lib/ 
-ENTRYPOINT java -jar /usr/lib/dbo-1.0-SNAPSHOT.jar --spring.profiles.active=it
--- src/main/java/com/acme/dbo/client/domain/Client.java
+++ src/main/java/com/acme/dbo/client/domain/Client.java
@@ -42,12 +42,14 @@ public class Client {
     String login;
 
     @EqualsAndHashCode.Exclude @Nullable @Size(min = 5, max = 128)
+    @JsonIgnore
     @ApiModelProperty(notes = "Client secret", example = "749f09bade8aca755660eeb17792da880218d4fbdc4e25fbec279d7fe9f65d70")
     @Column(name = "SECRET")
     String secret;
 
     @EqualsAndHashCode.Exclude @Nullable
     @ApiModelProperty(hidden = true)
+    @JsonIgnore
     @Column(name = "SALT")
     String salt;
 
