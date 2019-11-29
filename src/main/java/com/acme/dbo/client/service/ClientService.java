2695b45397cafaa82d32e14ce1441bb88e9b49fd
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2019-11-29 16:15:30 +0300

    Refactor to split out codebase.

--- src/main/java/com/acme/dbo/client/service/ClientService.java
+++
@@ -1,9 +0,0 @@
-package com.acme.dbo.client.service;
-
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.stereotype.Service;
-
-@ConditionalOnProperty(name = "features.client", havingValue = "true", matchIfMissing = true)
-@Service
-public class ClientService {
-}
